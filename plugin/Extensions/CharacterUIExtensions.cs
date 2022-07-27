659bf1676c5db3b8475e36d6fff3a49e2e2999f9
Faeryn <faeryn.an@gmail.com>
2022-07-27 19:10:15 +0200

    Use publicized fields

--- plugin/Extensions/CharacterUIExtensions.cs
+++ plugin/Extensions/CharacterUIExtensions.cs
@@ -1,16 +1,7 @@
-using System.Reflection;
-using HarmonyLib;
-using UnityEngine;
 using WeaponSkillKeys.UI;
 
 namespace WeaponSkillKeys.Extensions {
 	public static class CharacterUIExtensions {
-		private static readonly FieldInfo characterUI_hudCanvasGroup = AccessTools.Field(typeof(CharacterUI), "m_hudCanvasGroup");
-
-		public static CanvasGroup GetHUDCanvasGroup(this CharacterUI characterUI) {
-			return (CanvasGroup) characterUI_hudCanvasGroup.GetValue(characterUI);
-		}
-
 		public static WeaponSkillDisplayGroup GetWeaponSkillDisplayGroup(this CharacterUI characterUI) {
 			return characterUI.GetComponentInChildren<WeaponSkillDisplayGroup>();
 		}
--- plugin/Patches/CharacterUI.cs
+++ plugin/Patches/CharacterUI.cs
@@ -1,6 +1,5 @@
 using HarmonyLib;
 using UnityEngine;
-using WeaponSkillKeys.Extensions;
 using WeaponSkillKeys.UI;
 
 namespace WeaponSkillKeys.Patches {
@@ -8,7 +7,7 @@ namespace WeaponSkillKeys.Patches {
 	public class CharacterUI_Awake {
 		[HarmonyPostfix]
 		static void Postfix(CharacterUI __instance) {
-			GameObject hud = __instance.GetHUDCanvasGroup().gameObject;
+			GameObject hud = __instance.m_hudCanvasGroup.gameObject;
 			GameObject weaponSkillsObj = new GameObject("WeaponSkillDisplay");
 			weaponSkillsObj.transform.SetParent(hud.transform, false);
 			weaponSkillsObj.AddComponent<WeaponSkillDisplayGroup>();
