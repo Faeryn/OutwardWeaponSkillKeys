13bb85f333093d901e9a1643ce5251075a1b9a1a
Faeryn <faeryn.an@gmail.com>
2022-07-28 17:49:26 +0200

    New patch style

--- plugin/Patches/CharacterPatches.cs
+++ plugin/Patches/CharacterPatches.cs
@@ -3,36 +3,37 @@ using WeaponSkillKeys.Extensions;
 using WeaponSkillKeys.UI;
 
 namespace WeaponSkillKeys.Patches {
-	[HarmonyPatch(typeof(Character), nameof(Character.WeaponChanged))]
-	public class Character_WeaponChanged {
-		[HarmonyPostfix]
-		static void Postfix(Character __instance) {
+	[HarmonyPatch(typeof(Character))]
+	public class CharacterPatches {
+		[HarmonyPatch(nameof(Character.WeaponChanged)), HarmonyPostfix]
+		static void Character_WeaponChanged_Postfix(Character __instance) {
 			if (__instance.CharacterUI == null) {
 				return;
 			}
+
 			WeaponSkillDisplayGroup wsd = __instance.CharacterUI.GetWeaponSkillDisplayGroup();
 			if (wsd == null) {
 				return;
 			}
+
 			if (__instance.TryGetMainHandSkill(out Skill skill)) {
 				wsd.SetMainHandSkill(skill);
 			} else {
 				wsd.ClearMainHandSkill();
 			}
 		}
-	}
-
-	[HarmonyPatch(typeof(Character), nameof(Character.LeftHandChanged))]
-	public class Character_LeftHandChanged {
-		[HarmonyPostfix]
-		static void Postfix(Character __instance) {
+		
+		[HarmonyPatch(nameof(Character.LeftHandChanged)), HarmonyPostfix]
+		static void Character_LeftHandChanged_Postfix(Character __instance) {
 			if (__instance.CharacterUI == null) {
 				return;
 			}
+
 			WeaponSkillDisplayGroup wsd = __instance.CharacterUI.GetWeaponSkillDisplayGroup();
 			if (wsd == null) {
 				return;
 			}
+
 			if (__instance.TryGetOffHandSkill(out Skill skill)) {
 				wsd.SetOffHandSkill(skill);
 			} else {
@@ -40,5 +41,5 @@ namespace WeaponSkillKeys.Patches {
 			}
 		}
 	}
-	
+
 }
--- plugin/Patches/CharacterUIPatches.cs
+++ plugin/Patches/CharacterUIPatches.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 using WeaponSkillKeys.UI;
 
 namespace WeaponSkillKeys.Patches {
-	[HarmonyPatch(typeof(CharacterUI), "Awake")]
-	public class CharacterUI_Awake {
-		[HarmonyPostfix]
-		static void Postfix(CharacterUI __instance) {
+	[HarmonyPatch(typeof(CharacterUI))]
+	public class CharacterUIPatches {
+		[HarmonyPatch(nameof(CharacterUI.Awake)), HarmonyPostfix]
+		static void CharacterUI_Awake_Postfix(CharacterUI __instance) {
 			GameObject hud = __instance.m_hudCanvasGroup.gameObject;
 			GameObject weaponSkillsObj = new GameObject("WeaponSkillDisplay");
 			weaponSkillsObj.transform.SetParent(hud.transform, false);
